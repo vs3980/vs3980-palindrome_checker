dae11083adda50c1c5c4c80bcb4008014777cfc4
vs3980 <vs3980@srmist.edu.in>
2026-03-10 12:31:40 +0530

    uc1 first case

---
+++ src/uc1palindrome.java
@@ -0,0 +1,9 @@
+public class uc1palindrome {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Palindrome Checker Application");
+        System.out.println("Application Version: 1.0");
+        System.out.println("System initialized successfully.");
+
+    }
+}
